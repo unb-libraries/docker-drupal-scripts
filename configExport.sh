25fca7f4e41685479845fc0d7e82a5d1e02a51db
Jacob Sanford <jsanford@unb.ca>
2017-04-24 11:53:51 -0300

    Remove --skip-modules deprecated function
    https://github.com/drush-ops/drush/pull/2684

--- configExport.sh
+++ configExport.sh
@@ -3,5 +3,5 @@
 rm -rf ${DRUPAL_CONFIGURATION_DIR}/*
 
 # Write out config.
-drush --root=${DRUPAL_ROOT} --uri=default --yes config-export --skip-modules=${DRUPAL_CONFIGURATION_EXPORT_SKIP} --destination=${DRUPAL_CONFIGURATION_DIR}
+drush --root=${DRUPAL_ROOT} --uri=default --yes config-export --destination=${DRUPAL_CONFIGURATION_DIR}
 rm -rf ${DRUPAL_CONFIGURATION_DIR}/*devel*
