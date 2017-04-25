63c7d81788d4bb7e098743d0157553ffe2ca28a6
Jacob Sanford <jsanford@unb.ca>
2017-04-25 17:14:29 -0400

    Add upstream path to testing script

--- runCoreTests.sh
+++ runCoreTests.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 docker-compose build
 docker-compose up -d
-travis/waitForDeploy.sh
-travis/testDrupal.sh
-travis/generatePersistentContent.sh
+travis/upstream/waitForDeploy.sh
+travis/upstream/testDrupal.sh
+travis/upstream/generatePersistentContent.sh
 docker-compose stop
 sleep 30
 docker-compose up -d
-travis/waitForDeploy.sh
-travis/testDrupal.sh
-travis/testPersistentContent.sh
+travis/upstream/waitForDeploy.sh
+travis/upstream/testDrupal.sh
+travis/upstream/testPersistentContent.sh
 docker exec -i -t ${DOCKER_DRUPAL_CONTAINER_NAME} /scripts/runTests.sh
