bc7fec740e3335fe4906215d471d81f2c5ebc29e
Jacob Sanford <jsanford@unb.ca>
2017-05-10 09:52:27 -0300

    Update testDrupal.sh

--- testDrupal.sh
+++ testDrupal.sh
@@ -7,6 +7,6 @@ TESTS[1]='\Drupal\node\Tests\PageViewTest'
 TESTS[2]='\Drupal\system\Tests\File\UrlRewritingTest'
 TESTS[3]='\Drupal\file\Tests\FilePrivateTest'
 
-for TEST_CLASS in $TESTS; do
+for TEST_CLASS in "${TESTS[@]}"; do
   docker exec ${DOCKER_DRUPAL_CONTAINER_NAME} su nginx -s /bin/sh -c "php ${APP_WEBROOT}/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class '${TEST_CLASS}'"
 done
