d50b1cf49ff19b8125c1f98fdee8ed39459e355c
Jacob Sanford <jsanford@unb.ca>
2017-08-04 11:41:09 -0300

    IN-39 Migrate from version-specific newrelic installation in Drupal instances

--- installNewRelic.sh
+++ installNewRelic.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
-# Setup environment variables.
-NEWRELIC_PHP_BASE_FILE="newrelic-php5-${NEWRELIC_PHP_VERSION}-linux"
-NEWRELIC_DOWNLOAD_FILE="${NEWRELIC_PHP_BASE_FILE}-musl.tar.gz"
-NEWRELIC_DOWNLOAD_URI="http://download.newrelic.com/php_agent/archive/${NEWRELIC_PHP_VERSION}/${NEWRELIC_DOWNLOAD_FILE}"
+# Get latest download URI.
+NEWRELIC_DOWNLOAD_FILE=$(curl -s https://download.newrelic.com/php_agent/release/|grep -oP '(?<=/)newrelic-php5-[\d\.]+-linux-musl\.tar\.gz')
+NEWRELIC_DOWNLOAD_URI="https://download.newrelic.com/php_agent/release/$NEWRELIC_DOWNLOAD_FILE"
 NR_INSTALL_SILENT="true"
 
 # Install Package.
@@ -10,5 +9,5 @@ mkdir -p /opt/newrelic
 cd /opt/newrelic
 wget ${NEWRELIC_DOWNLOAD_URI} -O /opt/newrelic/${NEWRELIC_DOWNLOAD_FILE}
 tar -zxf ${NEWRELIC_DOWNLOAD_FILE}
-cd /opt/newrelic/${NEWRELIC_PHP_BASE_FILE}-${NEWRELIC_PHP_ARCH}
+cd /opt/newrelic/newrelic-php5-*
 sh newrelic-install install
