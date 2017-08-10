32a94dca3dd3c370caead86a58e45abf3f4765a8
Jacob Sanford <jsanford@unb.ca>
2017-08-10 09:37:37 -0300

    Ensure newrelic installs silently

--- installNewRelic.sh
+++ installNewRelic.sh
@@ -2,7 +2,6 @@
 # Get latest download URI.
 NEWRELIC_DOWNLOAD_FILE=$(curl -s http://download.newrelic.com/php_agent/release/ | sed -rn 's/.*(newrelic-php5-[0-9.]+-linux-musl\.tar\.gz).*/\1/p')
 NEWRELIC_DOWNLOAD_URI="http://download.newrelic.com/php_agent/release/$NEWRELIC_DOWNLOAD_FILE"
-NR_INSTALL_SILENT="true"
 
 # Install Package.
 mkdir -p /opt/newrelic
@@ -10,4 +9,4 @@ cd /opt/newrelic
 wget ${NEWRELIC_DOWNLOAD_URI} -O /opt/newrelic/${NEWRELIC_DOWNLOAD_FILE}
 tar -zxf ${NEWRELIC_DOWNLOAD_FILE}
 cd /opt/newrelic/newrelic-php5-*
-sh newrelic-install install
+NR_INSTALL_SILENT="true" sh newrelic-install install
