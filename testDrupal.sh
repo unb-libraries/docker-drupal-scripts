fb04c34b8fcd97b4cb859f47c1ea93e09439238a
Jacob Sanford <jsanford@unb.ca>
2017-08-23 13:15:14 -0300

    Uninstall simpletest after tests.

--- testDrupal.sh
+++ testDrupal.sh
@@ -10,3 +10,5 @@ TESTS[3]='\Drupal\file\Tests\FilePrivateTest'
 for TEST_CLASS in "${TESTS[@]}"; do
   docker exec ${DOCKER_DRUPAL_CONTAINER_NAME} su nginx -s /bin/sh -c "php ${APP_WEBROOT}/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class '${TEST_CLASS}'"
 done
+
+docker exec ${DOCKER_DRUPAL_CONTAINER_NAME} drush --root=${APP_WEBROOT} --uri=default en --yes simpletest
