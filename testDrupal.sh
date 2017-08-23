e84bd1938bba13f9fab6160b346ee121d74a722b
Jacob Sanford <jsanford@unb.ca>
2017-08-23 15:55:25 -0300

    Uninstall (not enable twice) simpletest

--- testDrupal.sh
+++ testDrupal.sh
@@ -11,4 +11,4 @@ for TEST_CLASS in "${TESTS[@]}"; do
   docker exec ${DOCKER_DRUPAL_CONTAINER_NAME} su nginx -s /bin/sh -c "php ${APP_WEBROOT}/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class '${TEST_CLASS}'"
 done
 
-docker exec ${DOCKER_DRUPAL_CONTAINER_NAME} drush --root=${APP_WEBROOT} --uri=default en --yes simpletest
+docker exec ${DOCKER_DRUPAL_CONTAINER_NAME} drush --root=${APP_WEBROOT} --uri=default pmu --yes simpletest
