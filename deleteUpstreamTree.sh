bb6a2f4534a76dda5758054bf3815933cf52719f
Jacob Sanford <jsanford@unb.ca>
2017-10-06 15:28:59 -0300

    Do not remove upstream drush

--- deleteUpstreamTree.sh
+++ deleteUpstreamTree.sh
@@ -4,5 +4,5 @@ set -e
 # Delete files from parent repo build, allowing a local build.
 rm -rf ${TMP_DRUPAL_BUILD_DIR}
 mkdir -p ${TMP_DRUPAL_BUILD_DIR}
-rm -f /usr/bin/drush
+
 rm -f /usr/bin/drupal
