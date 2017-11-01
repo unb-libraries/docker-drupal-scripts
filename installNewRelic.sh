192d337d8cf253d362ec0892db9273a6ef38fa45
Jacob Sanford <jsanford@unb.ca>
2017-11-01 13:18:09 -0300

    Tidy up temporary newrelic files

--- installNewRelic.sh
+++ installNewRelic.sh
@@ -12,3 +12,7 @@ wget ${NEWRELIC_DOWNLOAD_URI} -O /opt/newrelic/${NEWRELIC_DOWNLOAD_FILE}
 tar -zxf ${NEWRELIC_DOWNLOAD_FILE}
 cd /opt/newrelic/newrelic-php5-*
 NR_INSTALL_SILENT="true" sh newrelic-install install
+
+# Tidy up temporary files.
+rm -f /opt/newrelic/${NEWRELIC_DOWNLOAD_FILE}
+rm -f /tmp/nrinstall-*
